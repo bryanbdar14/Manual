a131e02b39e77240ce0083ef400ef6141318ec0a
bryanbdar14 <47328219+bryanbdar14@users.noreply.github.com>
2019-02-22 09:48:51 -0500

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+$('#mymodal').on('click', function () {
+    $('#mymodalshow').modal()
+});
+$('#mymodal1').on('click', function () {
+    $('#mymodalshow1').modal()
+});
+$('#mymodal2').on('click', function () {
+    $('#mymodalshow2').modal()
+});
+
